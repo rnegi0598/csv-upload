425901ea6dd58b5b9578c353ae77425051438c86
rnegi0598 <rnegi0598@gmail.com>
2023-08-29 13:46:38 +0530

    style improved

--- public/js/csv-upload.js
+++ public/js/csv-upload.js
@@ -1,10 +1,11 @@
 const fileInput=document.querySelector('#csv-upload');
 const selectedFile=document.querySelector('.selected-file');
-
+const submitButton=document.querySelector('.submit-wrapper button');
 fileInput.addEventListener('change',(event)=>{
     const uploadedFile=event.target.files[0];   
     if(selectedFile){
         selectedFile.innerHTML=uploadedFile.name;
+        submitButton.removeAttribute('disabled');
     }else{
         console.log('no file selected');
     }
